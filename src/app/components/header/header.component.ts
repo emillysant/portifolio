376741443a7ed8c1b832130bf13059a486aeeb54
Emilly-Technook <emilly@technook.com.br>
2023-06-21 10:43:00 -0300

    usando ngClass

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  route!: Subscription;
+  routeProject!: boolean;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.route = router.events.subscribe((route) => {
+      if (route instanceof NavigationEnd) {
+       console.log("rota projetos",this.router.url.includes('projects'))
+        this.routeProject = this.router.url.includes('projects')
+      }
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+   this.route.unsubscribe()
   }
 
   irParaTelaInicial() {
